d03aa9fa6e242e5b19d876e38393396f11800a96
Anthony <K2007798@students.katyisd.org>
2025-11-04 08:08:23 -0600

    hello

---
+++ src/Feeder.java
@@ -0,0 +1,69 @@
+package src;
+import java.util.*;
+public class Feeder {
+     private final ProgramState state;
+     private ArrayList<String> foodDice=new ArrayList<>();
+     public Feeder(ProgramState state){
+        this.state = state;
+     
+    
+    }
+
+    public boolean canReroll(){
+    if(foodDice.size()>1){
+        String one=foodDice.get(0);
+    for(int i=0;i<foodDice.size();i++){
+    if(!foodDice.get(i).equals(one)){
+        return false;
+    
+    }
+    }
+     
+    
+}
+   return true;
+    
+    }
+
+    public void reRoll(){
+    for(int i=0;i<5;i++){
+        int roll=(int)(Math.random()*6);
+    switch(roll){
+        case 0 -> foodDice.add("Seed");
+        case 1 -> foodDice.add("Fruit");
+        case 2 -> foodDice.add("Invertebrate");
+        case 3 -> foodDice.add("Fish");
+        case 4 -> foodDice.add("Rodent");
+        case 5 -> foodDice.add("Seed/Invertebrate");
+    }
+    }
+}
+
+    public void takeDie(int index,int player){
+    String food=foodDice.get(index);
+    switch(player){
+        case 1 -> {ArrayList<String> temp = state.playerOne.getFoodInHand();
+            temp.add(food);
+            state.playerOne.setFoodInHand(temp);
+        }
+         case 2 -> {ArrayList<String> temp = state.playerTwo.getFoodInHand();
+            temp.add(food);
+            state.playerTwo.setFoodInHand(temp);
+        }
+         case 3 -> {ArrayList<String> temp = state.playerThree.getFoodInHand();
+            temp.add(food);
+            state.playerThree.setFoodInHand(temp);
+        }
+         case 4 -> {ArrayList<String> temp = state.playerFour.getFoodInHand();
+            temp.add(food);
+            state.playerFour.setFoodInHand(temp);
+        }
+    }
+
+}
+}
+    
+
+
+
+
